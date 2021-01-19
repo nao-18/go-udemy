7151a255083d0e08be33b92d26c9caf276cc3bb6
nao-18 <kyahyo17@yahoo.co.jp>
2021-01-20 06:59:20 +0900

    tutorial_2

--- tutorial_2/main.go
+++ tutorial_2/main.go
@@ -1,10 +1,26 @@
 package main
 
+import "fmt"
+
 func main()  {
 	var i int = 1200
 
+	// 環境依存
 	//int8
 	//int16
 	//int32
 	//int64
+
+	var i2 int64 = 200
+	fmt.Println(i)
+
+	// 環境依存と明示的なintは計算できない
+	//fmt.Println(i+i2)
+
+	//型を表示する
+	fmt.Printf("%T\n", i2)
+	//cast
+	fmt.Printf("%T\n", int32(i2))
+	//cast and plus
+	fmt.Println(i + int(i2))
 }
