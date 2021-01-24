204bf68b1b9c5d620067999c39e12c8c9a654400
nao-18 <kyahyo17@yahoo.co.jp>
2021-01-24 16:41:34 +0900

    tutorial_11

---
+++ tutorial_11/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+//算術演算子
+
+func main()  {
+	fmt.Println(1 + 2)
+
+	fmt.Println(1 - 3)
+
+	fmt.Println(5 * 22)
+
+	fmt.Println(6 / 2)
+
+	n := 0
+	n += 4
+	n++
+	fmt.Println(n)
+	n--
+	fmt.Println(n)
+}
