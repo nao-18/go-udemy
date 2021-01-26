de77963ba372f358d1922eddb9aa945e65aff22f
nao-18 <kyahyo17@yahoo.co.jp>
2021-01-26 21:58:50 +0900

    tutorial_25

---
+++ tutorial_25/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+//ラベル付きfor
+
+func main()  {
+	//loop:
+	//for {
+	//	for {
+	//		for {
+	//			fmt.Println("START")
+	//			break loop
+	//		}
+	//		fmt.Println("処理をしない")
+	//	}
+	//	fmt.Println("処理をしない")
+	//}
+	//fmt.Println("END")
+
+	Loop:
+	for i := 0; i < 3; i++ {
+		for j := 1; j < 3; j++ {
+			if j > 1 {
+				continue Loop
+			}
+			fmt.Println(i, j, i*j)
+		}
+		fmt.Println("処理をしない")
+	}
+}
