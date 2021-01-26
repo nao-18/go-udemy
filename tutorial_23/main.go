cd6535165c9264fefd94be08577003fd6f2a9ef3
nao-18 <kyahyo17@yahoo.co.jp>
2021-01-26 21:11:24 +0900

    tutorial_23

---
+++ tutorial_23/main.go
@@ -0,0 +1,36 @@
+package main
+
+//switch
+//式スウィッチ
+
+func main() {
+	//n := 3
+	//ケースはカンマ区切りで書ける
+	//switch n {
+	//case 1, 2:
+	//	fmt.Println("1 or 2")
+	//case 3, 4:
+	//	fmt.Println("3 or 4")
+	//default:
+	//	fmt.Println("1 don't Know")
+	//}
+
+	//switch n := 2; n {
+	//case 1, 2:
+	//	fmt.Println("1 or 2")
+	//case 3, 4:
+	//	fmt.Println("3 or 4")
+	//default:
+	//	fmt.Println("1 don't Know")
+	//}
+
+	//n := 6
+	//switch {
+	//case n > 0 && n < 4:
+	//	fmt.Println("0 < n < 4")
+	//case n > 3 && n < 7:
+	//	fmt.Println("3 < n < 7")
+	//}
+
+
+}
