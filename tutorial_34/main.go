7d0b15638dc7b3edd78258da2ff76d66c09b03bb
nao-18 <kyahyo17@yahoo.co.jp>
2021-01-27 21:57:03 +0900

    tutorial_34

---
+++ tutorial_34/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+//スライス
+//可変長引数
+
+//Sum
+func Sum(s ...int) int {
+	n := 0
+	for _, v := range s {
+		n += v
+	}
+	return n
+}
+
+func main() {
+	fmt.Println(Sum(1, 2, 3))
+	fmt.Println(Sum(1, 2, 3, 4, 5, 6, 7))
+	fmt.Println(Sum())
+
+	sl := []int{1, 2, 3}
+	fmt.Println(Sum(sl...))
+}
